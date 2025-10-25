117df7ad4861dada505713f5f1a2ab2943610580
Fares <108934768+Faresincode@users.noreply.github.com>
2025-10-25 09:24:55 +0100

    Add files via upload

---
+++ Global.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+#include "clsUtil.h";
+
+using namespace std;
+
+short ScreenWidth = 50;
+string Green = clsUtil::Green();
+string White = clsUtil::White();
+string Blue = clsUtil::Blue();
+string Yellow = clsUtil::Yellow();
+string Red = clsUtil::Red();
+
+string FileSeparator = "#//#";
+string StudentsDormFileName = "StudentsDormData.txt";
+string StudentsDormComplaintsFileName = "StudentsDormComplaints.txt";
+string RoomsDataFileName = "RoomsData.txt";
+string EmployeeDataFileName = "DormEmployeeData.txt";
+string ScheduleFileName = "ShiftsSchedulesData.txt";
+string PaymentsDataFileName = "DormPaymentsData.txt";
+string MaintenanceRequestsFileName = "MaintenanceRequests.txt";
+string SecuritiesDataFileName = "SecuritiesData.txt";
+string PatrolSchedulesFileName = "PatrolSchedules.txt";
+string IncidentReportsFileName = "IncidentReports.txt";
+string VisitorsDataFileName = "VisitorsData.txt";
+string RegisterLoginsDataFileName = "RegisterLogins.txt";
+string LoginFileName = "LoginData.txt";
+
+
+
+
+
